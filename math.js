10b6c9444ee581d0fa90c09e5982d8a8ec36b055
barikassa <barikassa01@gmail.com>
2021-08-22 12:17:18 +0300

    first commit

---
+++ math.js
@@ -0,0 +1,67 @@
+//! 1. 
+// function getRandomNumber(num){
+//    var a = Math.random()*100;
+//     return a;
+// }
+// var result=getRandomNumber();
+//!2. 
+// function getRandomNumber(num){
+//    var b = Math.random()*10000;
+//     return b;
+// }
+// var result=getRandomNumber();
+//!3. 
+// function getRandomNumber(num){
+//     var c=Math.random()*55;
+//     return c;
+// }
+// var result=getRandomNumber();
+//!4. 
+
+// function getNumberFromUser(user){
+//     user=Math.random()*200
+// console.log(user);
+// }
+// var result=getNumberFromUser(56);
+
+//!with prompt
+// var userInput=Number(prompt("guess the your number"));
+// function getNumberFromUser(user){
+//     user=Math.random()*user;
+// console.log(user);
+// }
+// var result=getNumberFromUser(userInput);
+//!5. 
+// var userInput=Number(prompt("guess your number"));
+// function compareUserInputWithRandomNumber(user){
+//     compare=Math.floor(Math.random()*10);
+  
+//     if(user==compare){
+//         return "yes";
+//     }
+//     else{
+//         return "no"
+//     }
+// }
+// var result=compareUserInputWithRandomNumber(userInput);
+//!6. 
+// var userInput=Number(prompt("guess your number"))
+// function compareUserInputWithRandomNumber(user){
+//     compare=Math.ceil(Math.random*100);
+//     if(user==compare){
+//         return "yes"
+//     }
+//     return "sorry"
+// }
+// var result= compareUserInputWithRandomNumber();
+//!7. 
+// userName=prompt("win your game");
+// userNumber=Number(prompt("win your game"));
+// function getThewinner(firstName,num){
+//     compere=Math.floor(Math.random()*10)
+//  if(num==compere){
+//      return num + " you win";
+//  }
+//  return compere + " you lose";
+// }
+// var result= getThewinner(userName,userNumber);
